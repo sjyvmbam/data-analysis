151b0fd9dc2514313e581bf3c13d7ee6ac8b22f3
yvan mike <yvanmbami@yahoo.com>
2020-03-09 10:41:45 +0100

    new code to improve my programmation

---
+++ boucles.py
@@ -0,0 +1,51 @@
+continuer = "o"
+
+while continuer == "o":
+
+   print("On continue ?")
+
+   continuer=input("Voulez-vous continuer ? o/n ")
+
+   if continuer != "o":
+       print("aurevoir")
+
+
+
+'''nombres = [1, 21, 5, 44, 4, 9, 5, 83, 29, 31, 25, 38]
+
+nombres_pairs = [i for i in nombres if i % 2 == 0]
+
+nombres = range(-10, 10)
+
+nombres_positifs = [i for i in nombres if i >= 0]
+
+nombres = range(5)
+
+nombres_doubles = [i * 2 for i in nombres]
+
+nombres = range(10)
+
+nombres_inverses = [i if i % 2 == 0 else -i for i in nombres]
+
+b=input("veuillez saisir votre phrase")
+a=""
+
+for i in range(len(b)-1,-1,-1):
+    a+=b[i]
+    a==b
+print(a)
+
+
+
+utilisateur =0
+
+a=0
+
+while utilisateur<=10:
+
+  print(f"utilisateur",{a})
+
+  utilisateur+=1
+
+  a+=1
+  '''
